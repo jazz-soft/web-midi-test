f96caa04fcc66b5b0739c066ecdf10e59556e85f
Sema <sema@pyva.net>
2019-05-18 23:46:15 -0400

    Work in progress...

--- wmt.js
+++ wmt.js
@@ -2,20 +2,31 @@ function _readonly(obj, name, val) {
   Object.defineProperty(obj, name, { get: function() { return val; }, enumerable: true });
 }
 
-class MIDIAccess {
-  constructor(sysex) {
-    _readonly(this, 'inputs', []);
-    _readonly(this, 'outputs', []);
-    _readonly(this, 'sysexEnabled', sysex);
-  }
+function MidiSrc(name, man, ver) {
+  if (!(this instanceof MidiSrc)) return new MidiSrc(name, man, ver);
+  _readonly(this, 'name', '' + name);
+  _readonly(this, 'manufacturer', '' + man);
+  _readonly(this, 'version', '' + ver);
 }
 
-class DOMException {
-  constructor(name, message, code) {
-    _readonly(this, 'name', name);
-    _readonly(this, 'message', message);
-    _readonly(this, 'code', code);
-  }
+function MidiDst(name, man, ver) {
+  if (!(this instanceof MidiSrc)) return new MidiSrc(name, man, ver);
+  _readonly(this, 'name', '' + name);
+  _readonly(this, 'manufacturer', '' + man);
+  _readonly(this, 'version', '' + ver);
+}
+
+function MIDIAccess(sysex) {
+  _readonly(this, 'inputs', []);
+  _readonly(this, 'outputs', []);
+  _readonly(this, 'sysexEnabled', sysex);
+}
+
+function DOMException(name, message, code) {
+  this.name = name;
+  this.message = message;
+  this.code = code;
+  Object.freeze(this);
 }
 
 var _midi = true;
@@ -39,10 +50,13 @@ function requestMIDIAccess(arg) {
 }
 
 var WMT = {
+  MidiSrc: MidiSrc,
+  MidiDst: MidiDst,
   requestMIDIAccess: requestMIDIAccess
 };
 
 Object.defineProperty(WMT, 'midi', { get: function() { return _midi; }, set: function(b) { _midi = !!b; }, enumerable: true });
 Object.defineProperty(WMT, 'sysex', { get: function() { return _sysex; }, set: function(b) { _sysex = !!b; }, enumerable: true });
+Object.freeze(WMT);
 
 module.exports = WMT;
