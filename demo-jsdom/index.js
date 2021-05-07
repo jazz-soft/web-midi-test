ccf0f08c2ca43e7ff891a3238420bc507bd5b946
Sema <jazz-dev@jazz-soft.net>
2021-05-07 01:16:54 -0400

    Updated demos

--- demo-jsdom/index.js
+++ demo-jsdom/index.js
@@ -3,15 +3,36 @@ const WMT =  require('web-midi-test');
 
 const midi_in1 = new WMT.MidiSrc('VIRTUAL MIDI-In 1');
 const midi_in2 = new WMT.MidiSrc('VIRTUAL MIDI-In 2');
+const midi_in3 = new WMT.MidiSrc('VIRTUAL MIDI-In 3');
 const midi_out1 = new WMT.MidiDst('VIRTUAL MIDI-Out 1');
 const midi_out2 = new WMT.MidiDst('VIRTUAL MIDI-Out 2');
+const midi_out3 = new WMT.MidiDst('VIRTUAL MIDI-Out 3');
+
+midi_in3.busy = true;
+midi_out3.busy = true;
+
 midi_in1.connect();
 midi_in2.connect();
+midi_in3.connect();
 midi_out1.connect();
 midi_out2.connect();
+midi_out3.connect();
 
-JSDOM.fromFile('test.html', {
-  resources: 'usable',
-  runScripts: 'dangerously',
-  beforeParse: window => { window.navigator.requestMIDIAccess = WMT.requestMIDIAccess; }
-}).then(dom => { console.log('Thank you!'); });
+midi_out1.receive = function(msg) { console.log('VIRTUAL MIDI-Out 1 received:', msg); };
+midi_out2.receive = function(msg) { console.log('VIRTUAL MIDI-Out 2 received:', msg); };
+midi_out3.receive = function(msg) { console.log('VIRTUAL MIDI-Out 3 received:', msg); };
+
+function timeout(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+(async () => {
+  const dom = await JSDOM.fromFile('test.html', {
+    resources: 'usable',
+    runScripts: 'dangerously',
+    beforeParse: window => { window.navigator.requestMIDIAccess = WMT.requestMIDIAccess; }
+  });
+  await timeout(100);
+  midi_in1.emit([0x90, 0x60, 0x7f]);
+  midi_in2.emit([0x90, 0x60, 0x7f]);
+  midi_in3.emit([0x90, 0x60, 0x7f]);
+  await timeout(100);
+})();
--- demo-zombie/index.js
+++ demo-zombie/index.js
@@ -3,17 +3,31 @@ const WMT =  require('web-midi-test');
 
 const midi_in1 = new WMT.MidiSrc('VIRTUAL MIDI-In 1');
 const midi_in2 = new WMT.MidiSrc('VIRTUAL MIDI-In 2');
+const midi_in3 = new WMT.MidiSrc('VIRTUAL MIDI-In 3');
 const midi_out1 = new WMT.MidiDst('VIRTUAL MIDI-Out 1');
 const midi_out2 = new WMT.MidiDst('VIRTUAL MIDI-Out 2');
+const midi_out3 = new WMT.MidiDst('VIRTUAL MIDI-Out 3');
+
+midi_in3.busy = true;
+midi_out3.busy = true;
+
 midi_in1.connect();
 midi_in2.connect();
+midi_in3.connect();
 midi_out1.connect();
 midi_out2.connect();
+midi_out3.connect();
+
+midi_out1.receive = function(msg) { console.log('VIRTUAL MIDI-Out 1 received:', msg); };
+midi_out2.receive = function(msg) { console.log('VIRTUAL MIDI-Out 2 received:', msg); };
+midi_out3.receive = function(msg) { console.log('VIRTUAL MIDI-Out 3 received:', msg); };
 
 const browser = new Browser();
 browser.on('loaded', function(doc) {
   browser.window.navigator.requestMIDIAccess = WMT.requestMIDIAccess;
 });
 browser.visit('file://' + __dirname + '/test.html', function() {
-  console.log('### STATUS:', browser.status, 'URL:', browser.url);
+  midi_in1.emit([0x90, 0x60, 0x7f]);
+  midi_in2.emit([0x90, 0x60, 0x7f]);
+  midi_in3.emit([0x90, 0x60, 0x7f]);
 });
